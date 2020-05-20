d3007c615d0737283fa6ed3574105f03507066af
Chris Phillipson <fastnsilver@gmail.com>
2020-05-20 06:33:38 -0700

    Fix nginx-ingress experiment

--- experiments/k8s/nginx-ingress/nginx-ingress.tf
+++ experiments/k8s/nginx-ingress/nginx-ingress.tf
@@ -1,5 +1,10 @@
 module "nginx_ingress" {
   source = "../../../modules/nginx-ingress"
+
+  kubeconfig_path = var.kubeconfig_path
 }
 
-variable "kubeconfig_path" {}
+variable "kubeconfig_path" {
+  description = "The path to your .kubeconfig"
+  default = "~/.kubeconfig"
+}
