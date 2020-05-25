49c7a5fcde8d574fb7e82b726e8a4a0ad4fab9d6
Chris Phillipson <cphillipson@pivotal.io>
2020-05-25 13:05:01 -0700

    Fix experiments/azure/iam * support for creation and destruction of a service principal

--- experiments/azure/iam/create-iam.sh
+++ experiments/azure/iam/create-iam.sh
@@ -15,4 +15,4 @@ az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/${SUBSCRI
 az ad sp credential reset --name "aks-service-principal-for-${ENVIRONMENT_NAME}" --password "${CLIENT_SECRET}"
 
 echo "Take careful note of these new service principal credentials! Verify you can login with them by executing [ az login --service-principal -u {appId} --tenant {tenant} ]. You will be prompted to enter the {password}."
-echo "You will also want to generate a new private/public key pair. Execute [ ssh-keygen -t rsa -b 4096 -C {email-address} ].  Note the fully-qualified path to the public key."
+echo "You will also want to generate a new private/public key pair. Execute [ ssh-keygen -t rsa -b 4096 -C {email-address} ].  Replace {email-address} with a valid email address.  When complete, note the fully-qualified path to the public key."
--- experiments/azure/iam/destroy-iam.sh
+++ experiments/azure/iam/destroy-iam.sh
@@ -1,39 +1,13 @@
 #!/bin/bash
 
-SERVICE_ACCOUNT_NAME=terraform
+#!/bin/bash
+
+set -e
 
 if [ -z "$1" ]; then
-  SERVICE_ACCOUNT_NAME=terraform
-  PROJECT_ID=$(gcloud config get-value project)
-
-  gcloud projects remove-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role='roles/viewer'
-
-  gcloud projects remove-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role='roles/iam.serviceAccountUser'
-
-  gcloud projects remove-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role='roles/container.admin'
-
-  gcloud projects remove-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role='roles/container.clusterAdmin'
-
-  gcloud projects remove-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role='roles/container.developer'
-
-  gcloud projects remove-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role='roles/dns.admin'
-
-  gcloud iam service-accounts delete "${SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" --quiet
-else
-  PROJECT_ID="$1"
-  gcloud projects delete "$PROJECT_ID"
+	echo "Usage: destroy-iam.sh {environment-name}"
+	exit 1
 fi
 
-rm -f /tmp/gcp-${SERVICE_ACCOUNT_NAME}-${PROJECT_ID}-service-account-credentials.json
+ENVIRONMENT_NAME="$1"
+az ad sp delete --id "http://aks-service-principal-for-${ENVIRONMENT_NAME}"
