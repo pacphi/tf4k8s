ff0267113194e2c0445227112cea7c9f139170c1
Chris Phillipson <cphillipson@pivotal.io>
2020-05-26 14:40:00 -0700

    Update TAS4K8s to 0.2.0-build.101

--- ytt-libs/tas4k8s/scripts/download-tas4k8s.sh
+++ ytt-libs/tas4k8s/scripts/download-tas4k8s.sh
@@ -8,8 +8,8 @@ fi
 PIVNET_API_TOKEN="$1"
 pivnet login --api-token=$PIVNET_API_TOKEN
 
-TAS4K8S_VERSION="0.2.0-build.91"
-TAS4K8S_PRODUCT_FILE_ID=691880
+TAS4K8S_VERSION="0.2.0-build.101"
+TAS4K8S_PRODUCT_FILE_ID=692782
 pivnet download-product-files --product-slug='tas-for-kubernetes' --release-version="${TAS4K8S_VERSION}" --product-file-id="${TAS4K8S_PRODUCT_FILE_ID}"
 tar -xvf "tanzu-application-service.${TAS4K8S_VERSION}.tar" -C /tmp
 mkdir -p vendor
