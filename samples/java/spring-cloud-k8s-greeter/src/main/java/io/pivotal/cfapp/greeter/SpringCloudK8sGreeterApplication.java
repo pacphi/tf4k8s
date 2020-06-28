69ce9d6bb7b18358078bea35192509a54eb739a5
Chris Phillipson <cphillipson@pivotal.io>
2020-06-28 09:51:33 -0700

    Fix import

--- samples/java/spring-cloud-k8s-greeter/src/main/java/io/pivotal/cfapp/greeter/SpringCloudK8sGreeterApplication.java
+++ samples/java/spring-cloud-k8s-greeter/src/main/java/io/pivotal/cfapp/greeter/SpringCloudK8sGreeterApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.context.scope.refresh.RefreshScope;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
