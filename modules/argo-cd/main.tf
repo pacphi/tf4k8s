8b3b260e989b335d81cd6cc8b3d5434ed89c9498
Chris Phillipson <cphillipson@pivotal.io>
2020-07-16 06:40:02 -0700

    Update Helm chart for argo-cd to 2.5.4

--- modules/argo-cd/main.tf
+++ modules/argo-cd/main.tf
@@ -19,7 +19,7 @@ resource "helm_release" "argocd" {
   namespace  = kubernetes_namespace.argocd.metadata[0].name
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  version    = "2.3.6"
+  version    = "2.5.4"
 
   values = [data.template_file.argocd_config.rendered]
 
