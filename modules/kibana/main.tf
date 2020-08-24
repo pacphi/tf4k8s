e949814d5634ca488275abb73a7bc97dbe00399c
Chris Phillipson <cphillipson@pivotal.io>
2020-08-24 07:04:46 -0700

    Upgrade Kibana Helm chart to 7.8.1

--- modules/kibana/main.tf
+++ modules/kibana/main.tf
@@ -34,7 +34,7 @@ resource "helm_release" "kibana" {
   namespace  = var.namespace
   repository = "https://Helm.elastic.co"
   chart      = "kibana"
-  version    = "7.8.0"
+  version    = "7.8.1"
 
   values = [data.template_file.kibana_config.rendered]
 }
