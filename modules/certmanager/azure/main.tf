51be3af7f21d95157e7d3796daf97f58cf1b91d6
Chris Phillipson <cphillipson@pivotal.io>
2020-08-24 06:49:43 -0700

    Upgrade cert-manger Helm chart to 0.16.0

--- modules/certmanager/azure/main.tf
+++ modules/certmanager/azure/main.tf
@@ -53,7 +53,7 @@ resource "helm_release" "certmanager" {
   namespace  = kubernetes_namespace.certmanager.metadata[0].name
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
-  version    = "v0.15.2"
+  version    = "v0.16.0"
 
   set {
     name = "installCRDs"
