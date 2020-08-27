1f70d345807cdb78319460e9176c6ee6d13d69e0
Chris Phillipson <cphillipson@pivotal.io>
2020-08-27 06:45:44 -0700

    Upgrade Pivotal Cloud Service Broker release artifacts to most currently available

--- experiments/k8s/pivotal-csb/download-release-artifacts.sh
+++ experiments/k8s/pivotal-csb/download-release-artifacts.sh
@@ -6,14 +6,21 @@ mkdir -p ${DOWNLOADS_DIR}/aws
 mkdir -p ${DOWNLOADS_DIR}/azure
 mkdir -p ${DOWNLOADS_DIR}/gcp
 
-curl -Lo ${DOWNLOADS_DIR}/aws/cloud-service-broker https://github.com/pivotal/cloud-service-broker/releases/download/sb-0.1.0-rc.31-aws-0.0.1-rc.105/cloud-service-broker
-curl -Lo ${DOWNLOADS_DIR}/azure/cloud-service-broker https://github.com/pivotal/cloud-service-broker/releases/download/sb-0.1.0-rc.31-azure-0.0.1-rc.102/cloud-service-broker
-curl -Lo ${DOWNLOADS_DIR}/gcp/cloud-service-broker https://github.com/pivotal/cloud-service-broker/releases/download/sb-0.1.0-rc.31-gcp-0.0.1-rc.71/cloud-service-broker
+AZURE_RELEASE_VERSION=sb-0.1.0-rc.45-azure-0.0.1-rc.124
+AZURE_BROKERPAK_VERSION=0.0.1-rc.124
+AWS_RELEASE_VERSION=sb-0.1.0-rc.45-aws-0.0.1-rc.118
+AWS_BROKERPAK_VERSION=0.0.1-rc.118
+GCP_RELEASE_VERSION=sb-0.1.0-rc.45-gcp-0.0.1-rc.82
+GCP_BROKERPAK_VERSION=0.0.1-rc.82
 
-curl -Lo ${DOWNLOADS_DIR}/aws/aws-services.brokerpak https://github.com/pivotal/cloud-service-broker/releases/download/sb-0.1.0-rc.31-aws-0.0.1-rc.105/aws-services-0.0.1-rc.105.brokerpak
-curl -Lo ${DOWNLOADS_DIR}/azure/azure-services.brokerpak https://github.com/pivotal/cloud-service-broker/releases/download/sb-0.1.0-rc.31-azure-0.0.1-rc.102/azure-services-0.0.1-rc.102.brokerpak
-curl -Lo ${DOWNLOADS_DIR}/gcp/gcp-services.brokerpak https://github.com/pivotal/cloud-service-broker/releases/download/sb-0.1.0-rc.31-gcp-0.0.1-rc.71/google-services-0.0.1-rc.71.brokerpak
+curl -Lo ${DOWNLOADS_DIR}/aws/cloud-service-broker https://github.com/pivotal/cloud-service-broker/releases/download/${AWS_RELEASE_VERSION}/cloud-service-broker
+curl -Lo ${DOWNLOADS_DIR}/azure/cloud-service-broker https://github.com/pivotal/cloud-service-broker/releases/download/${AZURE_RELEASE_VERSION}/cloud-service-broker
+curl -Lo ${DOWNLOADS_DIR}/gcp/cloud-service-broker https://github.com/pivotal/cloud-service-broker/releases/download/${GCP_RELEASE_VERSION}/cloud-service-broker
 
-curl -Lo ${DOWNLOADS_DIR}/aws/version https://github.com/pivotal/cloud-service-broker/releases/download/sb-0.1.0-rc.31-aws-0.0.1-rc.105/version
-curl -Lo ${DOWNLOADS_DIR}/azure/version https://github.com/pivotal/cloud-service-broker/releases/download/sb-0.1.0-rc.31-azure-0.0.1-rc.102/version
-curl -Lo ${DOWNLOADS_DIR}/gcp/version https://github.com/pivotal/cloud-service-broker/releases/download/sb-0.1.0-rc.31-gcp-0.0.1-rc.71/version
+curl -Lo ${DOWNLOADS_DIR}/aws/aws-services.brokerpak https://github.com/pivotal/cloud-service-broker/releases/download/${AWS_RELEASE_VERSION}/aws-services-${AWS_BROKERPAK_VERSION}.brokerpak
+curl -Lo ${DOWNLOADS_DIR}/azure/azure-services.brokerpak https://github.com/pivotal/cloud-service-broker/releases/download/${AZURE_RELEASE_VERSION}/azure-services-${AZURE_BROKERPAK_VERSION}.brokerpak
+curl -Lo ${DOWNLOADS_DIR}/gcp/gcp-services.brokerpak https://github.com/pivotal/cloud-service-broker/releases/download/${GCP_RELEASE_VERSION}/google-services-${GCP_BROKERPAK_VERSION}.brokerpak
+
+curl -Lo ${DOWNLOADS_DIR}/aws/version https://github.com/pivotal/cloud-service-broker/releases/download/${AWS_RELEASE_VERSION}/version
+curl -Lo ${DOWNLOADS_DIR}/azure/version https://github.com/pivotal/cloud-service-broker/releases/download/${AZURE_RELEASE_VERSION}/version
+curl -Lo ${DOWNLOADS_DIR}/gcp/version https://github.com/pivotal/cloud-service-broker/releases/download/${GCP_RELEASE_VERSION}/version
