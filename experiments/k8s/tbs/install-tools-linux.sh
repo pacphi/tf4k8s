abc158f8c33cc0384fb16cdb572c4928d23e9f6b
Chris Phillipson <cphillipson@pivotal.io>
2020-09-01 20:37:22 -0700

    Update TBS CLI install scripts

--- experiments/k8s/tbs/install-tools-linux.sh
+++ experiments/k8s/tbs/install-tools-linux.sh
@@ -10,8 +10,8 @@ pivnet login --api-token=$PIVNET_API_TOKEN
 
 VERSION="1.0.2"
 
-PB_PRODUCT_FILE_ID=773505
-pivnet download-product-files --product-slug='build-service' --release-version="${VERSION}" --product-file-id="${PB_PRODUCT_FILE_ID}"
+KP_PRODUCT_FILE_ID=773505
+pivnet download-product-files --product-slug='build-service' --release-version="${VERSION}" --product-file-id="${KP_PRODUCT_FILE_ID}"
 mv kp-linux-* kp
 chmod +x kp
 sudo mv kp /usr/local/bin
