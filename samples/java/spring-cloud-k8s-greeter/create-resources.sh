cc0c91b4b9df001ae781e473da88a088f8e5ae9a
Chris Phillipson <cphillipson@pivotal.io>
2020-10-06 07:30:01 -0700

    Remove samples and source-repos directories

--- samples/java/spring-cloud-k8s-greeter/create-resources.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create namespace and configmap for this application to consume
-
-kubectl create namespace bitter-tears
-kubectl create configmap greetings-config --from-file=greetings-config.yml -n bitter-tears
-kubectl apply -f namespace-reader.yml
