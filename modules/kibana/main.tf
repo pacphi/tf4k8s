400af2a5ddc1dbb71ffcb7723ed57c18629d6170
Chris Phillipson <cphillipson@pivotal.io>
2020-10-20 16:41:09 -0700

    Upgrade Elasticsearch, Kibana and Metricbeat Helm charts to 7.9.2

--- modules/kibana/main.tf
+++ modules/kibana/main.tf
@@ -34,7 +34,7 @@ resource "helm_release" "kibana" {
   namespace  = var.namespace
   repository = "https://Helm.elastic.co"
   chart      = "kibana"
-  version    = "7.9.1"
+  version    = "7.9.2"
 
   values = [data.template_file.kibana_config.rendered]
 }
