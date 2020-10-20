2b71e89e74ad379b2cf98d748c253aa212370a90
Chris Phillipson <cphillipson@pivotal.io>
2020-10-20 16:53:26 -0700

    Upgrade Loki-stack Helm chart to 0.41.2

--- modules/loki-stack/main.tf
+++ modules/loki-stack/main.tf
@@ -33,7 +33,7 @@ resource "helm_release" "loki_stack" {
   namespace  = kubernetes_namespace.loki_stack.metadata[0].name
   repository = "https://grafana.github.io/loki/charts"
   chart      = "loki-stack"
-  version    = "0.40.1"
+  version    = "0.41.2"
 
   set {
     name = "fluent-bit.enabled"
