8836b8108a011539a4a2bac053a350d553ef9437
Chris Phillipson <cphillipson@pivotal.io>
2020-10-20 16:37:47 -0700

    Upgrade JFrog Artifactory Container Registry Helm chart to 3.2.0

--- modules/registry/jcr/main.tf
+++ modules/registry/jcr/main.tf
@@ -42,7 +42,7 @@ resource "helm_release" "jcr" {
   namespace  = kubernetes_namespace.jcr.metadata[0].name
   repository = "https://charts.jfrog.io"
   chart      = "artifactory-jcr"
-  version    = "2.5.1"
+  version    = "3.2.0"
 
   values = [data.template_file.jcr_config.rendered]
 
