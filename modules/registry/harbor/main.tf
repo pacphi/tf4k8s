94970fb93d39de0a94dc179e642e6d594db5b768
Chris Phillipson <cphillipson@pivotal.io>
2020-11-04 20:58:07 -0800

    Update Harbor Helm chart version to 1.5.1

--- modules/registry/harbor/main.tf
+++ modules/registry/harbor/main.tf
@@ -44,7 +44,7 @@ resource "helm_release" "harbor" {
   namespace  = kubernetes_namespace.harbor.metadata[0].name
   repository = "https://helm.goharbor.io"
   chart      = "harbor"
-  version    = "1.5.0"
+  version    = "1.5.1"
 
   values = [data.template_file.harbor_config.rendered]
 
