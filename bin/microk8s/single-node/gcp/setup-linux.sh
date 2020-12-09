e52255466d7035373a047187afded5c9df0122b4
Chris Phillipson <cphillipson@pivotal.io>
2020-12-09 08:28:59 -0800

    Upgrade Terraform version in BOM to 0.14.2

--- bin/microk8s/single-node/gcp/setup-linux.sh
+++ bin/microk8s/single-node/gcp/setup-linux.sh
@@ -17,7 +17,7 @@ HELMFILE_VERSION=0.119.1
 curl -Lo ./helmfile https://github.com/roboll/helmfile/releases/download/v${HELMFILE_VERSION}/helmfile_linux_amd64
 sudo mv helmfile /usr/local/bin
 
-TERRAFORM_VERSION=0.13.5
+TERRAFORM_VERSION=0.14.2
 wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 rm -f terraform_${TERRAFORM_VERSION}_linux_amd64.zip
