666ff732ce2711923a385e5fee6e8fcbc23183c6
warroyo <warroyo7199008@gmail.com>
2021-03-08 17:39:10 -0700

    updating tools and version

--- experiments/k8s/tbs/download-tbs.sh
+++ experiments/k8s/tbs/download-tbs.sh
@@ -16,7 +16,7 @@ pivnet login --api-token=$PIVNET_API_TOKEN
 
 cd /tmp || exit
 mkdir -p tbs-install
-TBS_VERSION="1.1.2"
+TBS_VERSION="1.1.3"
 TBS_PRODUCT_FILE_ID=899754
 pivnet download-product-files --product-slug='build-service' --release-version="${TBS_VERSION}" --product-file-id="${TBS_PRODUCT_FILE_ID}"
 mv build-service-${TBS_VERSION}.tar build-service.tar
--- experiments/k8s/tbs/install-tools-linux.sh
+++ experiments/k8s/tbs/install-tools-linux.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+set -e
 
 if [ -z "$1" ]; then
 	echo "Usage: install-tools-linux.sh {pivnet-api-token}"
 	exit 1
 fi
 
+curl -LO https://github.com/pivotal-cf/pivnet-cli/releases/download/v3.0.1/pivnet-linux-amd64-3.0.1
+chmod +x pivnet-linux-amd64-3.0.1
+sudo mv pivnet-linux-amd64-3.0.1 /usr/local/bin/pivnet
+
+
+
 PIVNET_API_TOKEN="$1"
 pivnet login --api-token=$PIVNET_API_TOKEN
 
-VERSION="1.1.2"
+VERSION="1.1.3"
 
 KP_PRODUCT_FILE_ID=883031
 pivnet download-product-files --product-slug='build-service' --release-version="${VERSION}" --product-file-id="${KP_PRODUCT_FILE_ID}"
 mv kp-linux-* kp
 chmod +x kp
 sudo mv kp /usr/local/bin
+
+curl -L https://carvel.dev/install.sh | sudo bash
