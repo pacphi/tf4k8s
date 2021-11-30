a80701c72b72c2ede80a5a283b0f4f603e8bb5af
Chris Phillipson <cphillipson@pivotal.io>
2021-11-29 21:18:05 -0800

    Add missing AWS region and set default to ECR module

--- modules/registry/ecr/vars.tf
+++ modules/registry/ecr/vars.tf
@@ -1,3 +1,8 @@
 variable "registry_name" {
   description = "Specifies the name of the Container Registry.  This name will be updated to append a unique suffix so as not to collide with a pre-existing registry."
 }
+
+variable "region" {
+  default = "us-west-2"
+  description = "A valid AWS region (e.g., us-east-1).  See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html#concepts-regions."
+}
