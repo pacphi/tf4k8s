0da77a0a1eb8b7583ff007dc4de7fad78aa26851
Chris Phillipson <cphillipson@pivotal.io>
2021-11-29 21:35:30 -0800

    Disambiguate endpoint from repository URL

--- modules/registry/ecr/outputs.tf
+++ modules/registry/ecr/outputs.tf
@@ -22,7 +22,12 @@ output "ecr_admin_password" {
   value = data.aws_ecr_authorization_token.token.password
 }
 
-output "ecr_url" {
-  description = "The URL that can be used to log into the container registry"
+output "ecr_endpoint" {
+  description = "The URL that can be used to log into the container image registry."
   value = data.aws_ecr_authorization_token.token.proxy_endpoint
+}
+
+output "ecr_repository_url" {
+  description = "The URL of the container image repository (in the form {aws_account_id}.dkr.ecr.region.amazonaws.com/{repository-name})."
+  value = aws_ecr_repository.ecr.repository_url
 }
